902f3834e4a8cb19c572f23247bebe446151d69f
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-11-10 09:19:50 +0000

    adding note

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(express.json())
 
 //Available Routes
 app.use('/api/auth',require('./routes/Auth'))
-app.use('/api/auth',require('./routes/Post'))
+// app.use('/api/post',require('./routes/Post'))
+app.use('/api/notes',require('./routes/Notes'))
 // app.use(require('./routes/User'))
 app.listen(port, () => {
   console.log(`EchelonAlumniApp 2.0 app listening at http://localhost:${port}/ `)
--- routes/Post.js
+++ routes/Post.js
@@ -4,6 +4,8 @@ const mongoose=require('mongoose');
 const requireLogin=require("../middleware/fetchUser");
 // const Post =mongoose.model("Post");
 const Post =require('../models/Post');
+const { body, validationResult } = require('express-validator');
+const Note = require('../models/Note');
 
 router.get("/allpost",requireLogin,(req,res)=>{
     Post.find()
@@ -28,27 +30,83 @@ router.get("/getsubpost",requireLogin,(req,res)=>{
     })
 })
 
-router.post("/createpost",requireLogin,(req,res)=>{
+// router.post("/createpost",requireLogin,(req,res)=>{
 
-    const {title,body,pic  }=req.body
-    if(!title ||!body||!pic){
-        return res.status(422).json({error:"Please add all the fields"})
+//     const {title,body,pic  }=req.body
+//     if(!title ||!body||!pic){
+//         return res.status(422).json({error:"Please add all the fields"})
+//     }
+//        req.user.password=undefined
+//     const post=new Post({
+//         title,
+//         body,
+//         photo:pic,
+//         postedBy:req.user
+//     })
+//     post.save().then(result=>{
+//         res.json({post:result})
+
+//     })
+//     .catch(err=>{
+//         console.log(err)
+//     })
+// })
+
+router.post("/createpost", requireLogin, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('body', 'Body must be at least 5 characters').isLength({ min: 5 }),
+    body('pic', 'Enter a valid pic URL').isURL(),
+], async (req, res) => {
+    try {
+        const { title, body, pic } = req.body;
+
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const post = new Post({
+            title,
+            body,
+            photo: pic,
+            postedBy: req.user.id
+        });
+
+        const savedPost = await post.save();
+
+        res.json({ post: savedPost });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
     }
-       req.user.password=undefined
-    const post=new Post({
-        title,
-        body,
-        photo:pic,
-        postedBy:req.user
-    })
-    post.save().then(result=>{
-        res.json({post:result})
+});
+router.post('/addnotedfff', requireLogin, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+        try {
 
+            const { title, description, tag } = req.body;
+
+            // If there are errors, return Bad request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
     })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+
 
 router.get("/mypost",requireLogin,(req,res)=>{
     Post.find({postedBy:req.user._id})
